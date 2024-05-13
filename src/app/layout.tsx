474f800f376b7d747e0c3ae338c2bd3fd7d7f94a
hata-junnosuke <jh19911209@icloud.com>
2024-05-13 22:38:54 +0900

    お試し化

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-import { Amplify } from 'aws-amplify';
-import config from '../amplifyconfiguration.json';
-Amplify.configure(config);
-
+import ConfigureAmplifyClientSide from '../components/ConfigureAmplifyClientSide';
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -20,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ConfigureAmplifyClientSide />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ src/components/ConfigureAmplifyClientSide.tsx
@@ -0,0 +1,11 @@
+// components/ConfigureAmplify.tsx
+// "use client";
+
+import { Amplify } from 'aws-amplify';
+import config from '@/amplifyconfiguration.json';
+
+Amplify.configure(config, { ssr: true });
+
+export default function ConfigureAmplifyClientSide() {
+  return null;
+}
